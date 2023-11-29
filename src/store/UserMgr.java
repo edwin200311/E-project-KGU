c2aab3f62faf27249abc6c44ec65161c9b6f3e29
71side-jin <150424711+71side-jin@users.noreply.github.com>
2023-11-30 05:13:15 +0900

    Update

--- src/store/UserMgr.java
+++ src/store/UserMgr.java
@@ -1,21 +1,17 @@
 package store;
 
-import java.util.List;
-
 import facade.DataEngineImpl;
-import mgr.Factory;
-import mgr.Manageable;
-import mgr.Manager;
 
-public class UserMgr extends DataEngineImpl<user> {
+public class UserMgr extends DataEngineImpl<User> {
 	private static UserMgr engine = null;
-	private UserMgr() {
-		setLabels(headers);
-	}
 	public static UserMgr getInstance() {
 		if (engine == null)
 			engine = new UserMgr();
 		return engine;
 	}
+	private UserMgr() {
+		setLabels(headers);
+	}
+	
 	private String[] headers = {"id", "point", "wait-point"};
 }
