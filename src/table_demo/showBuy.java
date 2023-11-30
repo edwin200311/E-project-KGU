06c1aba6be2d079326a4464c1918e362025bfe71
김지수 <150654374+sooddui@users.noreply.github.com>
2023-12-01 00:45:26 +0900

    Update showBuy.java
    전화번호 입력받는 부분 추가

--- src/table_demo/showBuy.java
+++ src/table_demo/showBuy.java
@@ -63,7 +63,7 @@ public class showBuy extends JPanel {
                 if(IDandAddress!=null){
                     String formattedDate = curDateTime.format(formatter);
                     StringBuilder result = new StringBuilder();
-                    result.append(IDandAddress[0]).append(" ").append(formattedDate).append(" ").append(IDandAddress[1]).append(" ").append(test);
+                    result.append(IDandAddress[0]).append(" ").append(formattedDate).append(" ").append(IDandAddress[1]).append(" ")..append(IDandAddress[2]).append(" ").append(test);
                     Rresult = result.toString().trim();
                     System.out.println(Rresult);
                 }
@@ -90,27 +90,31 @@ public class showBuy extends JPanel {
 
     }
 
-    // ID와 주소 입력 다이얼로그 표시하는 메서드
+    // ID와 주소 입력 다이얼로그 표시하는 메서드 + 전화번호
     private String[] showIDAndAddressInputDialog() {
-        JPanel panel = new JPanel(new GridLayout(2, 2));
+        JPanel panel = new JPanel(new GridLayout(3, 3));
         JTextField idField = new JTextField(10);
         JTextField addressField = new JTextField(10);
+        JTextField phoneField = new JTextField(10);
 
         panel.add(new JLabel("ID:"));
         panel.add(idField);
         panel.add(new JLabel("주소:"));
         panel.add(addressField);
+        panel.add(new JLabel("연락처:"));
+        panel.add(phoneField);
 
-        int result = JOptionPane.showConfirmDialog(null, panel, "ID와 주소 입력", JOptionPane.OK_CANCEL_OPTION);
+        int result = JOptionPane.showConfirmDialog(null, panel, "주문 정보 입력", JOptionPane.OK_CANCEL_OPTION);
 
         if (result == JOptionPane.OK_OPTION) {
             String enteredID = idField.getText();
             String enteredAddress = addressField.getText();
+            String enteredPhone = phoneField.getText();
 
-            if (!enteredID.isEmpty() && !enteredAddress.isEmpty()) {
-                return new String[]{enteredID, enteredAddress};
+            if (!enteredID.isEmpty() && !enteredAddress.isEmpty() && !enteredPhone.isEmpty()) {
+                return new String[]{enteredID, enteredAddress,enteredPhone};
             } else {
-                JOptionPane.showMessageDialog(null, "ID와 주소를 모두 입력하세요.", "입력 오류", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "정보를 모두 입력하세요.", "입력 오류", JOptionPane.ERROR_MESSAGE);
             }
         }
 
