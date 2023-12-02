9eb5ddb3544b768e322150821bc688b6b87bf9d5
edwin200311 <129483212+edwin200311@users.noreply.github.com>
2023-12-03 04:19:33 +0900

    .

--- src/table_demo/GUIMain.java
+++ src/table_demo/GUIMain.java
@@ -3,6 +3,8 @@ package table_demo;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.List;
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
@@ -28,6 +30,7 @@ import test.Main;
 public class GUIMain {
     // 싱글톤 패턴 적용 부분
     private static GUIMain main = null;
+
     public static GUIMain getInstance() {
         if (main == null)
             main = new GUIMain();
@@ -61,7 +64,7 @@ public class GUIMain {
         setupItemPane();
         setupOrderPane();
         // 아이템 리스트 탭과 주문 탭 두 개의 패널을 가지는 탭 패널
-        jtab.add("소비자.첫화면",mainPane);
+        jtab.add("소비자.첫화면", mainPane);
         jtab.add("소비자.꽃사전", scrollPane);
         jtab.add("소비자.주문", rorderPane);
         jtab.add("관리자.아이템", itemPane);
@@ -74,14 +77,14 @@ public class GUIMain {
     }
 
     private JPanel mainPane;
-    private void setupmainPane(){
+
+    private void setupmainPane() {
         mainPane = new JPanel(new BorderLayout());
         ImageIcon background = new ImageIcon("images/main.png");
-		background=GUIMain.getInstance().od.resize(background, 1280, 720);
+        background = GUIMain.getInstance().od.resize(background, 1280, 720);
         JLabel label = new JLabel(background);
-        mainPane.add(label,BorderLayout.CENTER);
+        mainPane.add(label, BorderLayout.CENTER);
     }
-    
 
     // 상품을 보여주는 패널 부분 - 탑과 JTable 포함
     private JPanel itemPane;
@@ -112,7 +115,7 @@ public class GUIMain {
         JPanel bottom = new JPanel(); // 디폴트 플로우레이아웃
         basketTable.tableTitle = "basket";
         basketTable.addComponentsToPane(OrderedItemMgr.getInstance());
-        
+
         bottom.add(basketTable, BorderLayout.CENTER);
         // 여기에 여러 가지 버튼을 넣을 수 있음 - 결재, 취소, 추가, 변경 등
         orderPane.add(bottom, BorderLayout.SOUTH);
@@ -120,6 +123,7 @@ public class GUIMain {
 
     private JPanel firstPane;
     private JScrollPane scrollPane = new JScrollPane();
+
     private void setupfirstPane() {
         firstPane = new JPanel(new BorderLayout());
         // 현재 꽃 42개-> Layout 14x3으로 설정
@@ -135,34 +139,35 @@ public class GUIMain {
     TableSelectionDemo rorderTable = new TableSelectionDemo();
     ItemTopPanel citemTop = new ItemTopPanel();
     orderDetail od = new orderDetail(null, null, null);
-    
+
     public String[] imagePaths = {
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
-        "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
+            "images/None.png",
     };
     showBuy sb = new showBuy(imagePaths);
+
     public void setuprorderPane() {
         rorderPane = new JPanel(new BorderLayout());
-        
-        //지금까지의 꽃 구매 현황 확인
+
+        // 지금까지의 꽃 구매 현황 확인
         JPanel west = new JPanel();
         sb = new showBuy(imagePaths);
-        west.add(sb,BorderLayout.CENTER);
+        west.add(sb, BorderLayout.CENTER);
 
         // 주문할 상품의 상세정보가 표를 선택할때마다 바뀌어서 출력
         JPanel east = new JPanel();
         ImageIcon icon = null;
         String imgName = "notChosen";
         icon = new ImageIcon("images/" + imgName + ".png");
-        if(imgName.equals("notChosen")){
-            icon=GUIMain.getInstance().od.resize(icon, 300, 300);
+        if (imgName.equals("notChosen")) {
+            icon = GUIMain.getInstance().od.resize(icon, 300, 300);
         }
         od = new orderDetail("", "", icon);
         east.add(od, BorderLayout.LINE_START);
@@ -170,26 +175,49 @@ public class GUIMain {
         // 주문할 상품을 검색하는 부분
         JPanel bottom = new JPanel();
         JPanel buyPane = new JPanel(new BorderLayout());
-        JScrollPane scrollPane=new JScrollPane();
+        JPanel fourpanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        JScrollPane scrollPane = new JScrollPane();
         buyPane.setLayout(new GridLayout(14, 3));
         List<?> itemlist = FlowerMgr.getInstance().search("");
-        for(Object o : itemlist) {
+        for (Object o : itemlist) {
             buyPane.add(new buyingImageCell((UIData) o));
         }
         scrollPane = new JScrollPane(buyPane);
-        scrollPane.setPreferredSize(new Dimension(600, 1000));
-        bottom.add(scrollPane,BorderLayout.CENTER);
+        scrollPane.setPreferredSize(new Dimension(450, 600));
+        bottom.add(scrollPane, BorderLayout.CENTER);
 
         rorderTable.tableTitle = "find";
         rorderTable.addComponentsToPane(FlowerMgr.getInstance());
         citemTop.setupTopPane(rorderTable);
-        //bottom.add(citemTop, BorderLayout.LINE_START);
-        //bottom.add(rorderTable, BorderLayout.LINE_END);
+        // bottom.add(citemTop, BorderLayout.LINE_START);
+        // bottom.add(rorderTable, BorderLayout.LINE_END);
 
-        west.setPreferredSize(new Dimension(600, 100));
-        east.setPreferredSize(new Dimension(300, 500));
-        rorderPane.add(west, BorderLayout.WEST);
-        rorderPane.add(bottom, BorderLayout.EAST);
-        rorderPane.add(east, BorderLayout.SOUTH);
+        west.setPreferredSize(new Dimension(100, 100));
+        // east.setPreferredSize(new Dimension(300, 500));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.5;
+        gbc.weighty = 0.5;
+        gbc.fill = GridBagConstraints.BOTH;
+        fourpanel.add(west, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 0.7;
+        gbc.weighty = 0.0; // 변경된 부분
+        fourpanel.add(bottom, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 0.3;
+        gbc.weighty = 1.0; // 변경된 부분
+        gbc.anchor = GridBagConstraints.NORTH;
+        fourpanel.add(east, gbc);
+
+        rorderPane.add(fourpanel, BorderLayout.CENTER);
+        // rorderPane.add(west, BorderLayout.WEST);
+        // rorderPane.add(bottom, BorderLayout.EAST);
+        // rorderPane.add(east, BorderLayout.SOUTH);
     }
 }
--- src/table_demo/buyingImageCell.java
+++ src/table_demo/buyingImageCell.java
@@ -8,29 +8,36 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
 import facade.UIData;
 
 public class buyingImageCell extends JPanel implements MouseListener {
+
 	ImageIcon imgIcon;
 	String name;
 	String price;
 	UIData item;
 	String[] userStrings;
+
 	buyingImageCell(UIData item) {
 		this.item = item;
 		setup();
 	}
+
 	void setup() {
 		setLayout(new BorderLayout());
 		userStrings = item.getUiTexts();
-		imgIcon = new ImageIcon("images/"+userStrings[0]+".png");
-		Image scaleImage = imgIcon.getImage().getScaledInstance(100, 70,Image.SCALE_DEFAULT);
+		imgIcon = new ImageIcon("images/" + userStrings[0] + ".png");
+		Image scaleImage = imgIcon.getImage().getScaledInstance(100, 70, Image.SCALE_DEFAULT);
 		ImageIcon imgResized = new ImageIcon(scaleImage);
 		add(new JLabel(imgResized), BorderLayout.CENTER);
 		String itemLabelStr = String.format("<html><body><h2>%s <br/>가격: %s원", userStrings[0], userStrings[5]);
@@ -42,20 +49,48 @@ public class buyingImageCell extends JPanel implements MouseListener {
 		add(itemLabel, BorderLayout.PAGE_END);
 		addMouseListener(this);
 	}
-    void addImage(){
-        
-    }
+
+	void addImage() {
+		String name = item.getUiTexts()[0];
+		String language = item.getUiTexts()[1];
+		String flowering = item.getUiTexts()[2];
+		String color = item.getUiTexts()[3];
+		String cost = item.getUiTexts()[4];
+		String care = item.getUiTexts()[5];
+		String icon = null;
+		ImageIcon imgicon = null;
+		Image image = null;
+
+		Font font = new Font("SansSerif", Font.PLAIN, 16); // 원하는 폰트, 스타일, 크기 설정
+				GUIMain.getInstance().od.textArea.setFont(font);
+				GUIMain.getInstance().od.textArea.setText("이름: " + name + "\n" + "꽃말: " + language + "\n" + "개화시기: "
+						+ flowering + "\n" + "색상: " + color + "\n" + "관리방법: " + cost + "\n" + "가격: " + care+"원");
+				icon = "images/" + name + ".png";
+				imgicon = new ImageIcon(icon);
+				image = imgicon.getImage();
+				GUIMain.getInstance().od.changeImg(imgicon);
+	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-       			
+		addImage();
 	}
+
 	@Override
-	public void mousePressed(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {
+	}
+
 	@Override
-	public void mouseReleased(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {
+	}
+
 	@Override
-	public void mouseEntered(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {
+	}
+
 	@Override
-	public void mouseExited(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {
+	}
+
 }
